688529af62823bdaa790aa5694ca0955a86cf000
Aleksanyaan <goharsimonyan5@gmail.com>
2023-06-13 18:38:09 +0400

    Cluster homework logic

--- index.js
+++ index.js
@@ -69,3 +69,41 @@
 // childProcess.on('close', () => {
 //     console.log('Child:closed');
 // });
+
+
+
+
+
+
+
+
+
+
+
+import cluster from 'cluster';
+
+if(cluster.isPrimary) {
+    //1. Get all the .csv file paths from a directory. reject if there is not correct path
+    //2. Determine the worker count based on available CPUs and csv files
+    //3. Distribute /csv file paths evenly among workers
+    //4. Fork workers and pass the .csv file path
+    //5. Collect process records count from workers
+    //6. Log the overall processed rcords count, and terminate (resolve)
+    
+    
+    //1) cluster.fork([])
+    //2) let worker = cluster.fork();
+    
+} else {
+    ///////////////7.///////////////// 
+    //1. Listen message and get the .csv file paths to process
+    //2. Read the content, parse (count process records)
+    //3. Save the parsed results as a json files
+    //4. Send the results (records count) to parent
+    //5. Terminate
+
+    //1) process.env
+    //2) worker. on('online', () => {
+    //     worker.send({});
+    // })
+}
