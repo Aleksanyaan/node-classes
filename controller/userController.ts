d38f636064ca682317e20ed780dd3c3ca22bdbda
Zhenya Aleksanyan <102899065+Aleksanyaan@users.noreply.github.com>
2023-07-10 15:52:35 +0400

    Update userController.ts

--- controller/userController.ts
+++ controller/userController.ts
@@ -1,16 +1,13 @@
-// import { UserService } from "../services/user.service";
+/ import { UserService } from "../services/user.service";
 import { IUserService } from "../services/user.service";
-
 export class UserControoller {
     constructor(private userService: IUserService) {}
     create(req, res, next) {
         const userPayLoad = req.body;
-
         try {
-            const result = IUserService.createUser(userPayLoad);
-
+            const result = this.userService.createUser(userPayLoad);
             res.status(201).send(result);
-        } catch (err) {
+        } catch (err:any) {
             // next(error);
             res.status(500).send({error: err.message});
         }
